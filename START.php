19d617367d705482a0d432d1f98b678599d7df18
iravasen <ivan.ravasenga@cern.ch>
2017-11-16 18:12:52 +0100

    home page

---
+++ START.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>HOME</title>
+
+  <!--Include CSS file-->
+  <link href="css/START.css" rel="stylesheet" type="text/css">  <!--Apply to all media-->
+</head>
+
+<body>
+	<h1> OB-Stave production database interface </h1> 
+  <!-- Navigation menu-->
+  <div id="navigation">
+    <ul class="navig-menu">
+			<li> <a href="tools/hicvisual.php"> HIC visual inspection (reception) </a></li>
+			<li> <a href="tools/cpvisual.php"> CP visual inspection </a></li>
+			<li> <a href="tools/recspaceframe.php"> Space frame visual inspection (reception) </a></li>
+			<li> <a href="tools/recpowerbus.php"> Power Bus inspection (reception) </a></li>
+			<li> <a href="tools/recbiasbus.php"> Bias Bus inspection (reception) </a></li>
+			<li> <a href="tools/recfilterboard.php"> Filter Board visual inspection (reception) </a></li>
+			<li> <a href="tools/recfpcext.php"> FPC extension reception test </a></li>
+      <li> <a href="tools/poweringaftcut.php"> HIC tab and wings cut </a></li>
+			<li> <a href="tools/hsassembly.php"> HS assembly (mask deposition and HIC gluing) </a></li>
+			<li> <a href="#"> Gluing of wire-bond protections (PB folding) </a></li>
+      <li> <a href="tools/solder.php"> Soldering/Desoldering (bridges, resistors, fpc extension)</a> </li>
+			<li> <a href="tools/hsgluing.php"> HS gluing on SF </a></li>
+			<li> <a href="tools/filterandcap.php"> Soldering of capacitors on PB and BB + soldering of FB to PB and BB </a></li>
+			<li> <a href="tools/solderPBBB.php">  PB + BB + Filter Board soldering to HS </a></li>
+      <li> <a href="tools/pbfold.php">  PB + BB folding on the HS </a></li>
+      <li> <a href="tools/staveship.php">  Stave shipment </a></li>
+    </ul>
+
+    <!--<img src="img/newits.png" alt="New ITS" style="width: 500px; height: 320px; margin-left: 600px;"/>-->
+
+  </div>
+
+</body>
+</html>
