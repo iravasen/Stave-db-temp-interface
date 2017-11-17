a427aa082971a198f94bd8ad988201ec12791573
iravasen <ivan.ravasenga@cern.ch>
2017-11-17 17:33:49 +0100

    new ordering of items

--- START.php
+++ START.php
@@ -8,26 +8,38 @@
 </head>
 
 <body>
-	<h1> OB-Stave production database interface </h1> 
+	<h1> OB-Stave production database interface </h1>
   <!-- Navigation menu-->
   <div id="navigation">
     <ul class="navig-menu">
-			<li> <a href="tools/hicvisual.php"> HIC visual inspection (reception) </a></li>
+
+			<h3> Reception tests of Stave components</h3>
 			<li> <a href="tools/cpvisual.php"> CP visual inspection </a></li>
-			<li> <a href="tools/recspaceframe.php"> Space frame visual inspection (reception) </a></li>
-			<li> <a href="tools/recpowerbus.php"> Power Bus inspection (reception) </a></li>
-			<li> <a href="tools/recbiasbus.php"> Bias Bus inspection (reception) </a></li>
-			<li> <a href="tools/recfilterboard.php"> Filter Board visual inspection (reception) </a></li>
+			<li> <a href="tools/recspaceframe.php"> Space frame visual inspection </a></li>
+			<li> <a href="tools/recpowerbus.php"> Power Bus inspection </a></li>
+			<li> <a href="tools/recbiasbus.php"> Bias Bus inspection </a></li>
+			<li> <a href="tools/recfilterboard.php"> Filter Board visual inspection </a></li>
 			<li> <a href="tools/recfpcext.php"> FPC extension reception test </a></li>
-      <li> <a href="tools/poweringaftcut.php"> HIC tab and wings cut </a></li>
+
+			<h3> HIC inspection and tests </h3>
+			<li> <a href="tools/hicvisual.php"> HIC visual inspection (reception) </a></li>
+			<li> <a href="tools/poweringaftcut.php"> HIC tab and wings cut </a></li>
+
+			<h3> HS </h3>
 			<li> <a href="tools/hsassembly.php"> HS assembly (mask deposition and HIC gluing) </a></li>
-			<li> <a href="#"> Gluing of wire-bond protections (PB folding) </a></li>
       <li> <a href="tools/solder.php"> Soldering/Desoldering (bridges, resistors, fpc extension)</a> </li>
+			<li> <a href="#"> Gluing of wire-bond protections (PB folding) --> Not implemented yet</a></li>
 			<li> <a href="tools/hsgluing.php"> HS gluing on SF </a></li>
+
+			<h3> Stave assembly </h3>
 			<li> <a href="tools/filterandcap.php"> Soldering of capacitors on PB and BB + soldering of FB to PB and BB </a></li>
 			<li> <a href="tools/solderPBBB.php">  PB + BB + Filter Board soldering to HS </a></li>
       <li> <a href="tools/pbfold.php">  PB + BB folding on the HS </a></li>
+
+			<h3> Storage and shipment </h3>
+			<li> <a href="tools/stavestorage.php"> Stave storage </a></li>
       <li> <a href="tools/staveship.php">  Stave shipment </a></li>
+
     </ul>
 
     <!--<img src="img/newits.png" alt="New ITS" style="width: 500px; height: 320px; margin-left: 600px;"/>-->
