ce7bcba85d6264ddab2ea53fafa981356d9a8096
iravasen <ivan.ravasenga@cern.ch>
2017-11-21 13:36:16 +0100

    intermediate page for bridge soldering

---
+++ tools/interm_page/bridge.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Soldering/desoldering</title>
+
+  <!--Include CSS file-->
+  <link rel="stylesheet" type="text/css" href="../../css/print.css" media="print" /> <!--For the printer-->
+	<link rel="stylesheet" type="text/css" href="../../css/fieldstyle.css"/>
+
+</head>
+
+<body class="special">
+
+	<a id="noprint" href="../../START.php"><img src="../../img/home.jpg" alt="Home" title="Home" width="100" height="100"/></a>
+  <br><br><br>
+
+	<p style="color: red; font-size: 14pt;"> Is it the first time that you perform this activity on this component?</p>
+	<a href="../solder.php"><input type="button" value="Yes"/></a>
+	<a href="../rework_pages/solder_rework.php"><input type="button" value="No"/></a>
+
+</body>
+</html>
