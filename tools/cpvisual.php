c1fdbbd373b064617a51d061388fa2dfe96c8918
iravasen <ivan.ravasenga@cern.ch>
2017-11-21 18:44:04 +0100

    improvements

--- tools/cpvisual.php
+++ tools/cpvisual.php
@@ -14,10 +14,14 @@
 	<a id="noprint" href="../START.php"><img src="../img/home.jpg" alt="Home" title="Home" width="100" height="100"/></a>
   <br><br><br>
 
-  <h1>Cold Plate visual inspection - Report</h1>
+  <h1>Cold Plate visual inspection and planarity - Report</h1>
 
-  <h2>Visual inspection</h2>
-  <br>
+	<fieldset>
+		<legend style="color: red; font-size: 14pt;"> Activity name</legend>
+			<span id="noprint"> (Example: OL/ML-CP-id planarity and inspection)</span>
+			<input type="text" placeholder="name" style="width: 500px">
+	</fieldset>
+
 	<form>
 	<fieldset>
 	<legend> Location </legend>
@@ -39,40 +43,87 @@
  <br>
 
 	<br>
-	<p> Cold-Plate ID: <input type="text" placeholder="HIC id"/> </p>
+	<p> Cold-Plate ID: <input type="text" placeholder="CP id"/> </p>
 	<br>
+
+	<h2>Visual inspection and planarity</h2>
+  <br>
 
 	<form action="">
 		<fieldset>
- 			<legend>Visible damages to CP structure (as cracks, damages to corners etc ...)?</legend><br>
- 			<input type="checkbox" name="Yes" value="Yes"/> Yes (add photo) <span> - Type: <input type="text" placeholder="Ins. type"/> </span>
+ 			<legend>Visible damages to CP carbon-fiber structure?</legend><br>
+			<input type="checkbox" name="No" value="No"/> No
+			<br />
+			<input id="check" type="checkbox" name="Yes" value="Yes"/> Yes
  			<br />
- 			<input type="checkbox" name="No" value="No"/> No
-			<?php
-			include('imagetool/imagetool.html');
-			?>
+
+			<fieldset id="ifproblem">
+				<span> Type of damage: </span> <br>
+				<input type="checkbox"/> Cracks in the carbon-fiber structure <br>
+				<input type="checkbox"/> Damages to corners <br>
+				<input type="checkbox"/> Damages on the edge <br>
+				<input type="checkbox"/> Other: <input type="text" placeholder="specify" style="width: 400px"/> <br>
+				<?php
+				include('imagetool/imagetool.html');
+				?>
+			</fieldset>
+
 		</fieldset>
 
 		<fieldset>
- 			<legend>Visible damages to cooling pipes along the CP?</legend><br>
- 			<input type="checkbox" name="Yes" value="Yes"/> Yes (add photo) <span> - Type: <input type="text" placeholder="Ins. type"/> </span>
- 			<br />
- 			<input type="checkbox" name="No" value="No"/> No
-			<?php
-			include('imagetool/imagetool.html');
-			?>
+ 			<legend>Visible damages to cooling pipes?</legend><br>
+			<input type="checkbox" name="No" value="No"/> No
+			<br />
+ 			<input id="check" type="checkbox" name="Yes" value="Yes"/> Yes
+
+			<fieldset id="ifproblem">
+				<span> Type of damages: </span> <br>
+				<input type="checkbox"/> Bending of the pipes <br>
+				<input type="checkbox"/> Visible cracks <br>
+				<input type="checkbox"/> Pipes detached from the carbon-fiber plate <br>
+				<input type="checkbox"/> Other: <input type="text" placeholder="specify" style="width: 400px"/> <br>
+				<?php
+				include('imagetool/imagetool.html');
+				?>
+			</fieldset>
+
 		</fieldset>
 
 		<fieldset>
 			<legend>Visible damages to external CP rubber tubes?</legend><br>
-			<input type="checkbox" name="Yes" value="Yes"/> Yes (add photo) <span> - Type: <input type="text" placeholder="Ins. type"/> </span>
- 			<br />
- 			<input type="checkbox" name="No" value="No"/> No
-			<?php
-			include('imagetool/imagetool.html');
-			?>
+			<input type="checkbox" name="No" value="No"/> No
+			<br />
+			<input id="check" type="checkbox" name="Yes" value="Yes"/> Yes
+
+			<fieldset id="ifproblem">
+				<span> Type of damages: </span> <br>
+				<input type="checkbox"/> Strong bending of the pipes <br>
+				<input type="checkbox"/> Visible cracks <br>
+				<input type="checkbox"/> Pipes damaged at the CP connection point <br>
+				<input type="checkbox"/> Other: <input type="text" placeholder="specify" style="width: 400px"/> <br>
+				<?php
+				include('imagetool/imagetool.html');
+				?>
+			</fieldset>
 		</fieldset>
+
+		<fieldset>
+			<legend> Is this Cold-Plate acceptable after the visual inspection? (if yes, measure planarity) </legend>
+			<input id="check" type="checkbox" name="Yes" value="Yes"/> Yes
+			<br />
+			<input type="checkbox" name="No" value="No"/> No
+
+			<fieldset id="ifproblem">
+				<span> Planarity: <input type="text" style="width: 90px"/> um</span><br>
+				<input type="checkbox"/> Acceptable <br>
+				<input type="checkbox"/> Acceptable, but we put it aside in case of need <br>
+				<input type="checkbox"/> Not acceptable <br>
+			</fieldset>
+
+		</fieldset>
+
 	</form>
+
 	<h2> Other comments </h2>
   <textarea rows="5" cols="100" name="modissection" placeholder="comments"></textarea>
 
@@ -83,6 +134,7 @@
 	?>
 
 	<input id="noprint" type="button" value="Save page" style="position: center" onClick="window.print()"/>
+	<a href="cpvisual.php" style="text-decoration: none"> <input type="button" value="Reset form"/></a>
 
 </body>
 </html>
