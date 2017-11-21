442b92714313914058b216f5e4f996ecd077a79a
iravasen <ivan.ravasenga@cern.ch>
2017-11-21 18:44:51 +0100

    added reset button

--- tools/solder.php
+++ tools/solder.php
@@ -494,7 +494,7 @@
 	?>
 
 	<input id="noprint" type="button" value="Save page" style="position: center" onClick="window.print()"/>
-
+	<a href="solder.php" style="text-decoration: none"> <input type="button" value="Reset form"/></a>
 
 </body>
 </html>
