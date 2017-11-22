aef04b6fed503a13576912a5ef564d1a9c821646
iravasen <ivan.ravasenga@cern.ch>
2017-11-22 17:16:38 +0100

    Intermediate page for activity with rework possibility

---
+++ tools/interm_page/fb_to_pb_bb.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>PB and BB to FB</title>
+
+  <!--Include CSS file-->
+  <link rel="stylesheet" type="text/css" href="../../css/print.css" media="print" /> <!--For the printer-->
+	<link rel="stylesheet" type="text/css" href="../../css/fieldstyle.css"/>
+
+</head>
+
+<body class="special">
+
+	<a id="noprint" href="../../START.php"><img src="../../img/home.jpg" alt="Home" title="Home" width="100" height="100"/></a>
+  <br><br><br>
+
+	<p style="color: red; font-size: 14pt;"> Is it the first time that you perform this activity on this component?</p>
+	<a href="../filterandcap.php"><input type="button" value="Yes"/></a>
+	<a href="../rework_pages/filterandcap_rework.php"><input type="button" value="No"/></a>
+
+</body>
+</html>
