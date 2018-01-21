ecbe7f5ffa9201488fff002ab8be101bfcb26bab
iravasen <ivan.ravasenga@cern.ch>
2018-01-21 18:26:35 +0100

    stave shipment intermediate

---
+++ tools/interm_page/staveship_interm.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Soldering/desoldering</title>
+
+  <!--Include CSS file-->
+  <link rel="stylesheet" type="text/css" href="../../css/print.css" media="print" /> <!--For the printer-->
+	<link rel="stylesheet" type="text/css" href="../../css/fieldstyle.css"/>
+
+</head>
+
+<body class="special">
+
+	<a id="noprint" href="../../START.php" style="text-decoration: none"> <input style=" font-size: 17pt" type="button" value="HOME page"/></a>
+  <br><br><br>
+
+	<p style="color: red; font-size: 14pt;"> Are you receiving or shipping a Stave?</p>
+	<a href="../stavereceive.php"><input type="button" value="Receiving"/></a>
+	<a href="../staveship.php"><input type="button" value="Shipping"/></a>
+
+</body>
+</html>
