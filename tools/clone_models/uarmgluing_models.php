1d1e03abc5ec13d76c05567eb199dac1829a90ad
iravasen <ivan.ravasenga@cern.ch>
2018-01-22 16:51:57 +0100

    clone models for u-arm gluing

---
+++ tools/clone_models/uarmgluing_models.php
@@ -0,0 +1,22 @@
+<div id="placeholder-hicdam">
+  <div id="template-hicdam" style="display: none">
+    <hr>
+    <ul>
+      <li> HIC in position: <input type="number" style="width: 60px"/> </li>
+      <li> Close to/ On chip number: <input type="number" style="width: 70px"/> </li>
+      <li> Describe the damage type <br>
+        <textarea cols="100" rows="10" placeholder="Describe"></textarea>
+      </li>
+      <li> Does it affect HIC functioning? <br>
+        <input type="checkbox"/> No <br>
+        <input type="checkbox"/> Yes <br>
+      </li>
+    </ul>
+
+    <?php
+    include('imagetool/imagetool.html');
+    ?>
+
+    <hr>
+  </div>
+</div>
