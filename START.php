8ec3f4e8d8ce40a6b257426ea056bf666a6c50de
iravasen <ivan.ravasenga@cern.ch>
2018-01-27 18:39:54 +0100

    changed links for stave storage, shipment, reception

--- START.php
+++ START.php
@@ -14,7 +14,7 @@
     <ul class="navig-menu">
 
 			<h3> Reception tests of Stave components</h3>
-			<li> <a href="tools/cpvisual.php"> CP visual inspection and planarity </a></li>
+			<li> <a href="tools/cpvisual.php"> CP visual inspection </a></li>
 			<li> <a href="tools/recspaceframe.php"> Space frame visual inspection </a></li>
 			<li> <a href="tools/recpowerbus.php"> Power Bus inspection </a></li>
 			<li> <a href="tools/recbiasbus.php"> Bias Bus inspection </a></li>
@@ -29,16 +29,16 @@
 			<li> <a href="tools/hsassembly.php"> HS assembly (mask deposition and HIC gluing) </a></li>
       <li> <a href="tools/interm_page/bridge.php"> Soldering/Desoldering (bridges, resistors, fpc extension)</a> </li>
 			<li> <a href="tools/uarmgluing.php"> U-arm gluing on HS</a></li>
-			<li> <a href="tools/hsgluing.php"> HS gluing on SF </a></li>
 
 			<h3> Stave assembly </h3>
+			<li> <a href="tools/hsgluing.php"> HSs gluing on SF (2 HSs) </a></li>
 			<li> <a href="tools/interm_page/fb_to_pb_bb.php"> Soldering of capacitors on PB and BB + soldering of FB to PB and BB </a></li>
 			<li> <a href="tools/interm_page/soldePBBBtoHS.php">  PB + BB + Filter Board soldering to Stave </a></li>
       <li> <a href="tools/pbfold.php">  PB + BB folding for Stave </a></li>
 
-			<h3> Storage and shipment </h3>
-			<li> <a href="tools/stavestorage.php"> Stave storage </a></li>
-      <li> <a href="tools/interm_page/staveship_interm.php">  Stave shipment </a></li>
+			<h3> Storage, shipment, reception </h3>
+			<li> <a href="tools/stavestorageship.php"> Stave storage and shipment </a></li>
+      <li> <a href="tools/stavereceive.php">  Stave reception </a></li>
 
     </ul>
 
